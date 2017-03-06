aebed4bcd3808de324deba561fa2cdb8893d703d
tangqi92 <imtangqi@gmail.com>
2017-03-06 17:29:35 +0800

    Initial Commit

---
+++ QTHandleOrientationChanges/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QTHandleOrientationChanges
+//
+//  Created by Tang Qi on 06/03/2017.
+//  Copyright © 2017 Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QTHandleOrientationChanges/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QTHandleOrientationChanges
+//
+//  Created by Tang Qi on 06/03/2017.
+//  Copyright © 2017 Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
